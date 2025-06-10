2250b1f4cc76d8f078cf373ca53e46948a5ba6f3
Frykttn <bernedoflor234@gmail.com>
2025-06-10 17:17:28 -0300

    agregue un ejemplo de condicional

--- js/ejercicios/01-centigradoAFahrenheit.js
+++ js/ejercicios/01-centigradoAFahrenheit.js
@@ -1,10 +1,9 @@
 //crear un programa que calcule cuantos grados fahrenheit son X grados centigrados. Formula (x °C × 9/5) + 32 =
 document.writeln('<h1>Ejercicio 1</h1>')
-let gradoCentigrado =parseInt(prompt('Ingresa los grados centigrados'))
+const gradoCentigrado =parseInt(prompt('Ingresa los grados centigrados'))
 const gradoFahrenheit = (gradoCentigrado * 9/5) + 32
 
-document.writeln('Los grados centigrados: '+gradoCentigrado+'°C, equivalen a:'+gradoFahrenheit+'°F')
+document.writeln('Los grados centigrados: '+gradoCentigrado+'°C, equivalen a: '+gradoFahrenheit+'°F')
 //ventana emergente para mostrar un mensaje
 alert('Los grados centigrados: '+gradoCentigrado+'°C, equivalen a: '+gradoFahrenheit+'°F')
 
-console.log(gradoCentigrado)
