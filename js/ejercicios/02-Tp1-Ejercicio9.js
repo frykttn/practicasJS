e44503cc59696fbefd3abe25c325f764720615e8
Frykttn <bernedoflor234@gmail.com>
2025-06-19 12:38:30 -0300

    agrego comentarios en la parte de variables

---
+++ js/ejercicios/02-Tp1-Ejercicio9.js
@@ -0,0 +1,9 @@
+//9- Escribe un programa que pida una frase y escriba las vocales que aparecen 
+//nota: Tener en cuenta la funcion lenght y substring o charAT (developer mozilla)
+
+let frase = prompt('Ingresa una frase')
+//hola mundo
+console.log(frase.length)
+console.log(frase.substring(0,3))
+console.log(frase.charAt(0))
+console.log(frase.charAt(5))
