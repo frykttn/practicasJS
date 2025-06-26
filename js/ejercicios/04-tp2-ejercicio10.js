71936e4d111b54e70961239015d4e0d02677c417
Frykttn <bernedoflor234@gmail.com>
2025-06-26 20:07:26 -0300

    seguimos realizando el ejercicio 10 del tp de bucles

---
+++ js/ejercicios/04-tp2-ejercicio10.js
@@ -0,0 +1,21 @@
+//Realiza un script que pida un numero de filas y columnas y escriba en una tabla. Dentro de cada una de las celdas debera escribirse un numero consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7x5 los numeros iran del 35 al 1.
+
+//pedir al usuario las filas y columnas
+
+//dibujar la tabla
+document.writeln(`
+  <table>
+    <tbody>
+`);
+document.writeln(`
+      <tr>
+        <td>35</td>
+      </tr>
+    </tbody>
+  </table>`);
+document.writeln(`
+    </tbody>
+  </table>
+`);
+
+//dibujar el valor de las celdas
