3d9692b779aef9fd1b3b43eee5126f9c6edaa12f
Frykttn <bernedoflor234@gmail.com>
2025-06-30 01:46:26 -0300

    completamos el punto 10 del tp de bucles

--- js/ejercicios/04-tp2-ejercicio10.js
+++ js/ejercicios/04-tp2-ejercicio10.js
@@ -1,21 +1,21 @@
 //Realiza un script que pida un numero de filas y columnas y escriba en una tabla. Dentro de cada una de las celdas debera escribirse un numero consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7x5 los numeros iran del 35 al 1.
 
 //pedir al usuario las filas y columnas
-const filas = parseInt(prompt('Ingresa un numero de filas'))
-const columnas = parseInt(prompt('Ingresa un numero de columnas'))
+const filas = parseInt(prompt("Ingresa un numero de filas"));
+const columnas = parseInt(prompt("Ingresa un numero de columnas"));
+let totalCelda = filas * columnas;
+console.log(filas, columnas);
 
-console.log(filas, columnas)
+console.log(filas, columnas);
 
 //dibujar la tabla
 document.writeln(`<table class='table table-striped'><tbody>`);
-for (let indiceFilas=0; indiceFilas < filas; indiceFilas++) {
-
-
-document.writeln(`<tr>`)
-for (let indiceColumnas =0; indiceColumnas < columnas; indiceColumnas++)
-document.writeln(`<td class='p-2'>35</td>`)
-document.writeln(`</tr>`)}
+for (let indiceFilas = 0; indiceFilas < filas; indiceFilas++) {
+  document.writeln(`<tr>`);
+  for (let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++)
+    document.writeln(`<td class='p-2'>${totalCelda--}</td>`);
+  document.writeln(`</tr>`);
+}
 document.writeln(`</tbody></table>`);
-
 
 //dibujar el valor de las celdas
