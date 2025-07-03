bbbbd7d4b9a08bd68e46f2a8ff5573621403c9b8
Frykttn <bernedoflor234@gmail.com>
2025-07-03 17:37:41 -0300

    vemos distintos tipos de metodos para rrays como: map, find, finIndex y filter

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -26,42 +26,71 @@ document.writeln(
   `<p>Mostrar el primer elemento del array: ${canciones[indice]}</p>`
 );
 
-mostrarCanciones('Mostrar el array de canciones')
+mostrarCanciones("Mostrar el array de canciones");
 
 // agregar elementos al array
-document.writeln(
-  '<h2 class="mt-3"></h2>'
-);
+document.writeln('<h2 class="mt-3"></h2>');
 canciones.unshift(false, "like stone");
-mostrarCanciones('Agregar un elemento al inicio del array')
-
-
+mostrarCanciones("Agregar un elemento al inicio del array");
 
 canciones.push("Like a Rolling stone");
-mostrarCanciones('Agregar un elemento al final de array')
-
-
+mostrarCanciones("Agregar un elemento al final de array");
 
 canciones.splice(4, 0, "feel good inc");
-mostrarCanciones('Agregar un elemento en el medio del array')
-
+mostrarCanciones("Agregar un elemento en el medio del array");
 
 //borrar elementos del array
 canciones.shift();
-mostrarCanciones("Borrar un elemento del inicio del array")
-
+mostrarCanciones("Borrar un elemento del inicio del array");
 
 //borrar un elemento del final del array
 canciones.pop();
-mostrarCanciones('Borrar un elemento del final del array')
-
+mostrarCanciones("Borrar un elemento del final del array");
 
 canciones.splice(2, 1);
 //canciones.splice(2,3) // desde la posicion 2 del array borro 3 elementos
 //canciones.splice(2) //borrar todo desde la posicion 2 del array
 //canciones.splice(canciones.length -1, 1)
-mostrarCanciones('Borrar un elemento del medio del array')
+mostrarCanciones("Borrar un elemento del medio del array");
 
 //modificar elementos del array
 canciones[3] = "Sobredosis de TV";
-mostrarCanciones('Modificar un elemento del array')
+mostrarCanciones("Modificar un elemento del array");
+
+// metodos extras para trabajar con arrays
+const nombreCancion = prompt("Ingresa un nombre de cancion");
+// find
+
+const cancionAlmacenada = canciones.find(
+  (cancion) => nombreCancion === cancion
+);
+console.log(cancionAlmacenada);
+
+//if (cancionAlmacenada) {
+//document.writeln("<p>La cancion buscada existe</p>");
+//} else {
+// document.writeln("<p>La cancion buscada no existe</p>");
+//}
+//operador ternario (condicion logica)? si se cumple la condicion hago esto : si no se cumple la condicon hago esto otro
+
+const respuesta = cancionAlmacenada
+  ? "La cancion existe"
+  : "La cancion no existe";
+document.writeln(`<p>${respuesta}</p>`);
+
+
+const indiceCancion = canciones.findIndex((cancion)=> nombreCancion === cancion )
+
+
+const indiceCancionBuscada = indiceCancion >-1
+  ? "La cancion esta en la posicion"+ indiceCancion + 'del array de canciones'
+  : "La cancion buscada no existe";
+document.writeln(`<p>${respuesta}</p>`);
+
+const cancionesFiltradas = canciones.filter((item)=> item.length >= 13 )
+
+console.log(cancionesFiltradas)
+
+document.writeln('<ul class="list-group">')
+cancionesFiltradas.map((cancion, indice )=>document.writeln(`<li class="list-group-item">${cancion} - indice ${indice}</li>`))
+document.writeln('</ul>')
